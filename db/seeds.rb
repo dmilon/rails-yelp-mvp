60d261ef5d8464bde8aef0f04836522c07f5ec89
Damien Milon <milon.damien@gmail.com>
2018-05-16 17:29:18 +0200

    solved 05/03/01

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Pittaya", address: "22 rue Oberkampf", phone_number: "0142382346", category: "belgian")
+Restaurant.create(name: "La Brigade", address: "36 rue Oberkampf", phone_number: "0145623226", category: "french")
+Restaurant.create(name: "Alicheur", address: "48 rue Oberkampf", phone_number: "0145982346", category: "italian")
+Restaurant.create(name: "MacDo", address: "54 rue Oberkampf", phone_number: "0142340986", category: "japanese")
+Restaurant.create(name: "Esprit Chai", address: "135 rue Oberkampf", phone_number: "0142549846", category: "chinese")
